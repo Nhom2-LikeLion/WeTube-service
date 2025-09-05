9a35374e9a7df74f09813e347bb15d0de1efa8bc
khaitrank366 <khaitran.k366@gmail.com>
2025-09-05 16:32:32 +0700

    Update File

---
+++ src/main/java/com/wetube/wetube_service/entity/channel/Subscription.java
@@ -0,0 +1,31 @@
+package com.wetube.wetube_service.entity.channel;
+
+import com.wetube.wetube_service.entity.CompositeKey.SubscriptionId;
+import com.wetube.wetube_service.enumeration.SubscriptionType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+
+    @EmbeddedId
+    private SubscriptionId id;
+
+    @Enumerated(EnumType.STRING)
+    private SubscriptionType notificationMode;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Channel getChannel() {
+        return id.getTier().getChannel();
+    }
+}
