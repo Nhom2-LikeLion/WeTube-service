92a5c250eb3cbb1cc8ff71ccacc63749e21a6085
TaiTran <avetai2k3@gmail.com>
2025-09-17 15:03:35 +0700

    "seed"

---
+++ src/main/resources/db/Video_playlist.sql
@@ -0,0 +1,12 @@
+INSERT INTO videos (id, title, video_url, thumbnail_url, duration,  total_view, created_at, updated_at)
+VALUES
+('12121212-1111-1111-1111-111111111111', 'Despacito', 'https://www.youtube.com/watch?v=kJQP7kiw5Fk', 'https://img.youtube.com/vi/kJQP7kiw5Fk/hqdefault.jpg', 282, 0, NOW(), NOW()),
+('23232323-2222-2222-2222-222222222222', 'Shape of You', 'https://www.youtube.com/watch?v=JGwWNGJdvx8', 'https://img.youtube.com/vi/JGwWNGJdvx8/hqdefault.jpg', 263, 0, NOW(), NOW()),
+('34343434-3333-3333-3333-333333333333', 'See You Again', 'https://www.youtube.com/watch?v=RgKAFK5djSk', 'https://img.youtube.com/vi/RgKAFK5djSk/hqdefault.jpg', 229, 0, NOW(), NOW()),
+('46464646-4444-4444-4444-444444444444', 'Uptown Funk', 'https://www.youtube.com/watch?v=OPf0YbXqDm0', 'https://img.youtube.com/vi/OPf0YbXqDm0/hqdefault.jpg', 270, 0, NOW(), NOW()),
+('52525252-5555-5555-5555-555555555555', 'Gangnam Style', 'https://www.youtube.com/watch?v=9bZkp7q19f0', 'https://img.youtube.com/vi/9bZkp7q19f0/hqdefault.jpg', 252, 0, NOW(), NOW()),
+('69696969-6666-6666-6666-666666666666', 'Sugar', 'https://www.youtube.com/watch?v=09R8_2nJtjg', 'https://img.youtube.com/vi/09R8_2nJtjg/hqdefault.jpg', 305, 0, NOW(), NOW()),
+('73737373-7777-7777-7777-777777777777', 'Roar', 'https://www.youtube.com/watch?v=CevxZvSJLk8', 'https://img.youtube.com/vi/CevxZvSJLk8/hqdefault.jpg', 272, 0, NOW(), NOW()),
+('86868686-8888-8888-8888-888888888888', 'Counting Stars', 'https://www.youtube.com/watch?v=hT_nvWreIhg', 'https://img.youtube.com/vi/hT_nvWreIhg/hqdefault.jpg', 257, 0, NOW(), NOW()),
+('91919191-9999-9999-9999-999999999999', 'Thinking Out Loud', 'https://www.youtube.com/watch?v=lp-EO5I60KA', 'https://img.youtube.com/vi/lp-EO5I60KA/hqdefault.jpg', 281, 0, NOW(), NOW()),
+('anananan-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'Let Her Go', 'https://www.youtube.com/watch?v=RBumgq5yVrA', 'https://img.youtube.com/vi/RBumgq5yVrA/hqdefault.jpg', 254, 0, NOW(), NOW());
