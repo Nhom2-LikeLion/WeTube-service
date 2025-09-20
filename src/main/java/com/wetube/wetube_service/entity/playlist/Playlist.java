70138684084c093728a1c4546f09068468b9487a
TaiTran <avetai2k3@gmail.com>
2025-09-20 22:34:26 +0700

    .

--- src/main/java/com/wetube/wetube_service/entity/playlist/Playlist.java
+++ src/main/java/com/wetube/wetube_service/entity/playlist/Playlist.java
@@ -1,7 +1,7 @@
 package com.wetube.wetube_service.entity.playlist;
 
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -56,17 +56,17 @@ public class Playlist {
     private String title;
 
     @CreationTimestamp
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    private LocalDate updatedAt;
+    private LocalDateTime updatedAt;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PlaylistType playlistType;
     
     @Column(nullable = false)
-    private String privacy;
+    private Boolean privacy;
 
     @Column(nullable = true)
     private String thumbnailUrl;
