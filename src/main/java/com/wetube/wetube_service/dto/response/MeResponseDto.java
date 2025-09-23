477388795b9f99fb344fabade5de745650457046
Kira366 <khaitran.k366@gmail.com>
2025-09-23 14:33:46 +0700

    update MeApi

--- src/main/java/com/wetube/wetube_service/dto/response/MeResponseDto.java
+++ src/main/java/com/wetube/wetube_service/dto/response/MeResponseDto.java
@@ -1,6 +1,15 @@
 package com.wetube.wetube_service.dto.response;
 
+import com.wetube.wetube_service.dto.response.playlist.UserPlaylistDto;
+
 import java.util.List;
 
-public record MeResponseDto(String sub, String email, String picture, String name, List<String> roles) {
-}
+public record MeResponseDto(
+        String sub,
+        String email,
+        String picture,
+        String name,
+        List<String> roles,
+        UserChannelResponseDto channel,
+        List<UserPlaylistDto> playlists
+) { }
--- src/main/java/com/wetube/wetube_service/mapper/MeMapper.java
+++ src/main/java/com/wetube/wetube_service/mapper/MeMapper.java
@@ -5,12 +5,14 @@ import com.wetube.wetube_service.entity.AppUser;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { PlaylistMapper.class })
 public interface MeMapper {
     @Mapping(target = "sub", expression = "java(user.getId() != null ? user.getId().toString() : null)")
     @Mapping(target = "email", source = "email")
     @Mapping(target = "picture", source = "picture")
     @Mapping(target = "name", source = "name")
     @Mapping(target = "roles", expression = "java(new java.util.ArrayList<>(user.getRoleCodes()))")
+    @Mapping(target = "channel", source = "channel")
+    @Mapping(target = "playlists", source = "playlists")
     MeResponseDto toDto(AppUser user);
 }
