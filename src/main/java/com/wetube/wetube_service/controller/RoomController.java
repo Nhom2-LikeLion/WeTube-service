274cae38c128caaf3b0a7f538571b9c73a36a6d0
Kira366 <khaitran.k366@gmail.com>
2025-09-23 14:53:31 +0700

    fix room

--- src/main/java/com/wetube/wetube_service/controller/RoomController.java
+++ src/main/java/com/wetube/wetube_service/controller/RoomController.java
@@ -1,9 +1,5 @@
 package com.wetube.wetube_service.controller;
 
-import com.wetube.wetube_service.dto.room.playlist.VideoStateChangeRequest;
-import com.wetube.wetube_service.dto.room.room.JoinRoomRequest;
-import com.wetube.wetube_service.dto.room.room.Room;
-import com.wetube.wetube_service.dto.room.room.WatchMember;
 import com.wetube.wetube_service.service.impl.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -30,43 +26,43 @@ public class RoomController {
     private final RoomService roomService;
     private final SimpMessagingTemplate messagingTemplate;
 
-    @GetMapping
-    public ResponseEntity<Map<String, Room>> getAllRooms(){
-        return ResponseEntity.ok(roomService.getRooms());
-    }
+//    @GetMapping
+//    public ResponseEntity<Map<String, Room>> getAllRooms(){
+//        return ResponseEntity.ok(roomService.getRooms());
+//    }
+//
+//    @MessageMapping("/room/create")
+//    public void handleCreateRoom(@Payload String username,
+//                                 SimpMessageHeaderAccessor headerAccessor) {
+//        System.out.println("Received username: " + username);
+//
+//        WatchMember host = new WatchMember();
+//        host.setUsername(username);
+//        host.setHost(true);
+//
+//        Room room = roomService.createRoom(host);
+//
+//        String sessionId = headerAccessor.getSessionId();
+//
+//        // Gửi trực tiếp cho người tạo phòng
+//        messagingTemplate.convertAndSendToUser(
+//                sessionId,
+//                "/queue/room/created",
+//                room,
+//                createHeaders(sessionId)
+//        );
+//    }
 
-    @MessageMapping("/room/create")
-    public void handleCreateRoom(@Payload String username,
-                                 SimpMessageHeaderAccessor headerAccessor) {
-        System.out.println("Received username: " + username);
 
-        WatchMember host = new WatchMember();
-        host.setUsername(username);
-        host.setHost(true);
-
-        Room room = roomService.createRoom(host);
-
-        String sessionId = headerAccessor.getSessionId();
-
-        // Gửi trực tiếp cho người tạo phòng
-        messagingTemplate.convertAndSendToUser(
-                sessionId,
-                "/queue/room/created",
-                room,
-                createHeaders(sessionId)
-        );
-    }
-
-
-    @MessageMapping("/room/join")
-    @SendTo("/topic/room.{roomId}.members")
-    public void handleJoinRoom(@Payload JoinRoomRequest request,
-                               SimpMessageHeaderAccessor headerAccessor) {
-        Room updatedRoom = roomService.addMember(request.getRoomId(), request.getUsername());
-
-        // Gửi thông tin room cập nhật cho tất cả client trong topic
-        messagingTemplate.convertAndSend("/topic/room/" + request.getRoomId() + "/members", updatedRoom);
-    }
+//    @MessageMapping("/room/join")
+//    @SendTo("/topic/room.{roomId}.members")
+//    public void handleJoinRoom(@Payload JoinRoomRequest request,
+//                               SimpMessageHeaderAccessor headerAccessor) {
+//        Room updatedRoom = roomService.addMember(request.getRoomId(), request.getUsername());
+//
+//        // Gửi thông tin room cập nhật cho tất cả client trong topic
+//        messagingTemplate.convertAndSend("/topic/room/" + request.getRoomId() + "/members", updatedRoom);
+//    }
 
     private MessageHeaders createHeaders(String sessionId) {
         SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
--- src/main/java/com/wetube/wetube_service/service/impl/RoomService.java
+++ src/main/java/com/wetube/wetube_service/service/impl/RoomService.java
@@ -1,9 +1,9 @@
 package com.wetube.wetube_service.service.impl;
 
-import com.wetube.wetube_service.dto.room.room.JoinRoomRequest;
-import com.wetube.wetube_service.dto.room.room.Room;
-import com.wetube.wetube_service.dto.room.playlist.VideoStateChangeRequest;
-import com.wetube.wetube_service.dto.room.room.WatchMember;
+//import com.wetube.wetube_service.dto.room.room.JoinRoomRequest;
+//import com.wetube.wetube_service.dto.room.room.Room;
+//import com.wetube.wetube_service.dto.room.playlist.VideoStateChangeRequest;
+//import com.wetube.wetube_service.dto.room.room.WatchMember;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -21,39 +21,39 @@ import java.util.concurrent.ConcurrentHashMap;
 @Setter
 @Slf4j
 public class RoomService {
-    private final Map<String, Room> rooms = new ConcurrentHashMap<>();
-
-    public Room createRoom(WatchMember host) {
-
-        String roomId = generateFriendlyRoomId();
-
-        Room room = new Room();
-        room.setRoomId(roomId);
-        room.getMembers().add(host);
-
-        rooms.put(roomId, room);
-
-        log.debug("Room created: {}", room);
-        return room;
-    }
-
-    public Room addMember(String roomId, String username) {
-        Room room = rooms.get(roomId);
-        if (room == null) return null;
-
-        boolean exists = room.getMembers().stream()
-                .anyMatch(m -> m.getUsername().equals(username));
-
-        if (!exists) {
-            WatchMember member = new WatchMember();
-            member.setUsername(username);
-            member.setHost(false);
-            room.getMembers().add(member);
-        }
-
-        log.debug("Member added: {} to room {}", username, roomId);
-        return room;
-    }
+//    private final Map<String, Room> rooms = new ConcurrentHashMap<>();
+//
+//    public Room createRoom(WatchMember host) {
+//
+//        String roomId = generateFriendlyRoomId();
+//
+//        Room room = new Room();
+//        room.setRoomId(roomId);
+//        room.getMembers().add(host);
+//
+//        rooms.put(roomId, room);
+//
+//        log.debug("Room created: {}", room);
+//        return room;
+//    }
+//
+//    public Room addMember(String roomId, String username) {
+//        Room room = rooms.get(roomId);
+//        if (room == null) return null;
+//
+//        boolean exists = room.getMembers().stream()
+//                .anyMatch(m -> m.getUsername().equals(username));
+//
+//        if (!exists) {
+//            WatchMember member = new WatchMember();
+//            member.setUsername(username);
+//            member.setHost(false);
+//            room.getMembers().add(member);
+//        }
+//
+//        log.debug("Member added: {} to room {}", username, roomId);
+//        return room;
+//    }
 
 
     private String generateFriendlyRoomId() {
