62d00cecd7fdc19792ca54da07677ad5b5aab940
SMT310 <shinizeam@gmail.com>
2025-09-23 23:02:49 +0700

    Fix MeMapper & MeResponseDto

--- src/main/java/com/wetube/wetube_service/mapper/MeMapper.java
+++ src/main/java/com/wetube/wetube_service/mapper/MeMapper.java
@@ -12,6 +12,7 @@ public interface MeMapper {
     @Mapping(target = "picture", source = "picture")
     @Mapping(target = "name", source = "name")
     @Mapping(target = "roles", expression = "java(new java.util.ArrayList<>(user.getRoleCodes()))")
+    @Mapping(target = "channelId", expression = "java(user.getChannel() != null && user.getChannel().getId() != null ? user.getChannel().getId().toString() : null)")
     @Mapping(target = "channel", source = "channel")
     @Mapping(target = "playlists", source = "playlists")
     MeResponseDto toDto(AppUser user);
