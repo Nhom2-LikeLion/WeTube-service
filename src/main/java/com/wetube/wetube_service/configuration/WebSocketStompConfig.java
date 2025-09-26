f04ff5353e5e53d609e07aa27183c6a6b3c19bc1
SMT310 <shinizeam@gmail.com>
2025-09-26 17:13:54 +0700

    Fix configureMessageConverters

--- src/main/java/com/wetube/wetube_service/configuration/WebSocketStompConfig.java
+++ src/main/java/com/wetube/wetube_service/configuration/WebSocketStompConfig.java
@@ -22,11 +22,11 @@ public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
 
     }
 
-//    @Override
-//    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
 //        messageConverters.add(new ProtobufMessageConverter());
-//        return false;
-//    }
+        return true;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
