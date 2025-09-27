41067d4d9e0179a397898e953c1a5ce95a6f9bde
Kira366 <khaitran.k366@gmail.com>
2025-09-28 03:54:05 +0700

    upate add member

--- src/main/java/com/wetube/wetube_service/controller/RoomController.java
+++ src/main/java/com/wetube/wetube_service/controller/RoomController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -55,16 +56,33 @@ public class RoomController {
 
     @MessageMapping("/room/member/{roomId}")
     @SendTo("/topic/rooms/member/{roomId}")
-    public WatchMember handleMemberRoom(@DestinationVariable String roomId,
-                               @Payload Map<String, String> body ) {
+    public List<WatchMember> handleMemberRoom(@DestinationVariable String roomId,
+                                              @Payload Map<String, String> body) {
         String username = body.get("username");
-        WatchMember member = new WatchMember();
-        member.setUsername(username);
-        member.setHost(false);
-        roomService.addMember(roomId,username);
-        System.out.println("Received member in room " + roomId + ": " + member);
-        return member;
+        System.out.println("Request to ADD member in room " + roomId + ": " + username);
+        Room room = roomService.getRooms().get(roomId);
+        System.out.println("Members BEFORE add: " + room.getMembers());
+        List<WatchMember> updatedMembers = roomService.addMember(roomId, username).getMembers();
+        System.out.println("Members AFTER add: " + updatedMembers);
+
+        return updatedMembers;
     }
+
+
+    @MessageMapping("/room/member/leave/{roomId}")
+    @SendTo("/topic/rooms/member/{roomId}")
+    public List<WatchMember> handleRemoveMember(@DestinationVariable String roomId,
+                                                @Payload Map<String, String> body) {
+        String username = body.get("username");
+        System.out.println("Request to REMOVE member in room " + roomId + ": " + username);
+        Room room = roomService.getRooms().get(roomId);
+        System.out.println("Members BEFORE remove: " + room.getMembers());
+        List<WatchMember> updatedMembers = roomService.removeMember(roomId, username).getMembers();
+        System.out.println("Members AFTER remove: " + updatedMembers);
+
+        return updatedMembers;
+    }
+
 
     @MessageMapping("/chat/{roomId}")
     @SendTo("/topic/rooms/chat/{roomId}")
